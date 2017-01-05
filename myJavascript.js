23193d09fa350c25af32331bfa0412a7076fb273
Ben Conner <ben.conner40@gmail.com>
2017-01-05 11:55:43 -0600

    finished project

--- myJavascript.js
+++ myJavascript.js
@@ -24,13 +24,39 @@ var randomUser = {
 }
 
 //handlebars step one: grap the html from the script tag
-
+var userHTML = $("#userScript").html()
 
 //handlebars step two: compile it into a template
+var userTemplate = Handlebars.compile(userHTML)
 
-
-//handlebars step three:render the HTML by passing the data to the template
-
+//handlebas step three: render the resulting HTML by passing the data to the template
+var userOutput = userTemplate(randomUser)
 
 //handlebars step four: put the complete HTML into the DOM
+$("#userDisplay").append(userOutput)
 
+var promise = new Promise(function(resolve, reject){
+    $.ajax({
+      url: 'https://randomuser.me/api/',
+      dataType: 'json',
+      success: function(data) {
+        console.log(data);
+        resolve(data)
+      }
+    });
+})
+promise
+.then(
+    function(data){
+        var randUserHTML = $("#randUserScript").html()
+
+        //handlebars step two: compile it into a template
+        var randUserTemplate = Handlebars.compile(userHTML)
+
+        //handlebas step three: render the resulting HTML by passing the data to the template
+        var userOutput = userTemplate(data)
+
+        //handlebars step four: put the complete HTML into the DOM
+        $("#userDisplay").append(userOutput)
+    }
+)
